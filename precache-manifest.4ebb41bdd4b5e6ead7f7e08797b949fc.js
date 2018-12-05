884852a4f53ccffe98bd628c2a1587847da6cc05
xiaoqu ju <sam@xiaoqus-imac.home>
2018-12-05 20:37:41 +1100

    Updates

---
+++ precache-manifest.4ebb41bdd4b5e6ead7f7e08797b949fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "78808f597653f0ffd811",
+    "url": "/contactmanager/static/js/main.78808f59.chunk.js"
+  },
+  {
+    "revision": "8048f28cc922927f5eb5",
+    "url": "/contactmanager/static/js/1.8048f28c.chunk.js"
+  },
+  {
+    "revision": "78808f597653f0ffd811",
+    "url": "/contactmanager/static/css/main.c662a5b3.chunk.css"
+  },
+  {
+    "revision": "8048f28cc922927f5eb5",
+    "url": "/contactmanager/static/css/1.ed1112f0.chunk.css"
+  },
+  {
+    "revision": "a707187cb04b889ac07efd02e3ea8964",
+    "url": "/contactmanager/index.html"
+  }
+];
